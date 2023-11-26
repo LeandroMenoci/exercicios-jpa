229d17ca2d20a7e9c6934aa2b18359d2ef1a3d51
leandromenoci <menoci.leandro@gmail.com>
2023-11-26 18:09:20 -0300

    Obtendo vários usuarios de uma tabela

--- src/teste/basico/ObterUsuarios.java
+++ src/teste/basico/ObterUsuarios.java
@@ -1,5 +1,33 @@
 package teste.basico;
 
-public class ObterUsuarios {
+import modelo.basico.Usuario;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.TypedQuery;
+import java.util.List;
+
+public class ObterUsuarios {
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("exercicios-jpa");
+        EntityManager em = emf.createEntityManager();
+
+        String jpql = "select u from Usuario u";
+        TypedQuery<Usuario> query = em.createQuery(jpql, Usuario.class);
+        query.setMaxResults(5);
+
+//        List<Usuario> usuarios = em.createQuery("select u from Usuario u", Usuario.class)
+//                .setMaxResults(5)
+//                .getResultList();
+
+        List<Usuario> usuarios = query.getResultList();
+
+        for(Usuario usuario : usuarios) {
+            System.out.println("ID: " + usuario.getId() + " / E-mail: " + usuario.getEmail());
+        }
+
+        em.close();
+        emf.close();
+    }
 }
